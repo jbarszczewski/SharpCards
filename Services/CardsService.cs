7cece26a14a1e97bebc26894d16348e7fcc62ea1
jbarszczewski <jakub.barszczewski@outlook.com>
2022-05-17 13:22:36 +0200

    add download and upload cards from json

--- Services/CardsService.cs
+++ Services/CardsService.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using SharpCards.Database;
 using SharpCards.Models;
 
@@ -47,5 +50,24 @@ public class CardsService
 	{
 		_ = await _database.DeleteByKey<string, CardDto>(cardGuid.ToString());
 		Cards.RemoveAt(Cards.IndexOf(Cards.First(c => c.Id == cardGuid)));
+	}
+
+	public Stream GetCardsStream()
+	{
+		var jsonString = JsonSerializer.Serialize(Cards);
+		return new MemoryStream(Encoding.Default.GetBytes(jsonString));
+	}
+
+	public async Task AddCardsFromJson(string input)
+	{
+		var cards = JsonSerializer.Deserialize<IList<CardDto>>(input) ?? Enumerable.Empty<CardDto>();
+		var newCards = cards.Where(c => Cards.All(c2 => c.Id != c2.Id)).ToList();
+		_ = await _database.AddItems<CardDto>(newCards);
+		foreach (var card in newCards)
+		{
+			Cards.Add(Card.FromDto(card));
+		}
+
+		Console.WriteLine($"{newCards.Count} new cards added");
 	}
 }
